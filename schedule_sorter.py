615da6a7cdfc0baf97fa6e6f0d1e14bc45e9ced5
Dexter <60551416+Santerias@users.noreply.github.com>
2024-12-05 20:25:07 +0100

    testing lesson status

--- schedule_sorter.py
+++ schedule_sorter.py
@@ -46,6 +46,16 @@ def get_todays_lessons(lessons):
     todays_lessons.sort(key=lambda x: datetime.fromisoformat(x["startDate"]))
     return todays_lessons
 
+def get_lesson_status(lessons, lessonId, week):
+    now = datetime.now().timestamp()
+    lessons.sort(key=lambda x: datetime.fromisoformat(x["startDate"]))
+    for lesson in lessons:
+        if "studentLessonStatus" in lesson:
+            student_status = lesson["studentLessonStatus"]
+            if student_status["week"] == week and student_status["lessonId"] == lessonId:
+                return f"Lesson ID: {student_status["lessonId"]}\nWeek: {student_status["week"]}\nComment: {student_status["comment"]}\nAbsence: {student_status["absence"]}\nAttendance: {student_status["name"]}\nReason: {student_status["reason"]}\n"
+    return None
+
 
 print(f"Previous Lesson: {get_previous_lesson(data)}")
 print(f"Current Lesson: {get_current_lesson(data)}")
@@ -55,3 +65,6 @@ print(f"\nToday's lessons: {get_todays_lessons(data)}")
 print("\nToday's lessons (pretty printed):")
 for lesson in get_todays_lessons(data):
     print(f"{lesson["name"]} at {lesson["startDate"]} to {lesson["endDate"]}\n")
+
+print("Status for GYAREE:\n")
+print(get_lesson_status(data, 379377, 35))
