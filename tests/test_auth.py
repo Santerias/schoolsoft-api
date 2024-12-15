de21b922c0e9598043adc762b2d0ab89183081fd
Piman <pimanroya06@gmail.com>
2024-12-15 17:49:05 +0100

    feat: added two simple tests for authentication

---
+++ tests/test_auth.py
@@ -0,0 +1,21 @@
+import pytest
+import schoolsoft
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+username = getenv("USER")
+password = getenv("PASS")
+school = getenv("SCHOOL")
+
+
+def test_invalid_credentials():
+    with pytest.raises(schoolsoft.exceptions.InvalidCredentials):
+        api = schoolsoft.Api("aksodkaopsdkpo", "aksdpkoasdkpoa", "nti")
+        api.utils.get_next_lesson()  # call method that requires authentication
+
+
+def test_valid_credentials():
+    api = schoolsoft.Api(username, password, school)
+    api.utils.get_next_lesson()
