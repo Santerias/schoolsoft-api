d701317086dd4ed503c867bd9c29183f466b537d
Piman <pimanroya06@gmail.com>
2024-12-21 19:31:44 +0100

    fix: renamed methods to correct naming convention

--- schoolsoft/calendar.py
+++ schoolsoft/calendar.py
@@ -36,20 +36,20 @@ class Calendar:
     def get_store(self) -> Store:
         return Store.from_dict(self.api._request("get", "/calendar/student/stores"))
 
-    def fetch_student_resource(self) -> dict:
+    def get_student_resource(self) -> dict:
         return self.api._request("get", "/calendar/student/resource")
 
-    def fetch_student_language(self) -> dict:
+    def get_student_language(self) -> dict:
         return self.api._request("get", "/calendar/student/language")
 
-    def fetch_student_news(self) -> dict:
+    def get_student_news(self) -> dict:
         return self.api._request("get", "/calendar/student/news")
 
-    def fetch_student_version(self) -> dict:
+    def get_student_version(self) -> dict:
         return self.api._request("get", "/calendar/student/version")
 
-    def fetch_student_plannings(self) -> dict:
+    def get_student_plannings(self) -> dict:
         return self.api._request("get", "/calendar/student/plannings")
 
-    def fetch_student_time_bookings(self) -> dict:
+    def get_student_time_bookings(self) -> dict:
         return self.api._request("get", "/calendar/student/time_bookings")
